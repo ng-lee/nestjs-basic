fb3910cae6e39f09e6ddaac2860869c62e96b4c9
ng-lee <nhd5607@gmail.com>
2025-09-01 18:28:37 +0900

    custom repository decorator 추가

---
+++ src/common/typeorm.module.ts
@@ -0,0 +1,39 @@
+import { DynamicModule, Provider } from '@nestjs/common';
+import { TYPEORM_CUSTOM_REPOSITORY } from './typeorm.decorator';
+import { getDataSourceToken } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
+export class TypeormModule {
+  public static forCustomRepository<T extends new (...args: any[]) => any>(
+    repositories: T[],
+  ): DynamicModule {
+    const providers: Provider[] = [];
+
+    for (const repository of repositories) {
+      const entity = Reflect.getMetadata(TYPEORM_CUSTOM_REPOSITORY, repository);
+
+      if (!entity) {
+        continue;
+      }
+
+      providers.push({
+        inject: [getDataSourceToken()],
+        provide: repository,
+        useFactory: (dataSource: DataSource): typeof repository => {
+          const baseRepository = dataSource.getRepository<any>(entity);
+          return new repository(
+            baseRepository.target,
+            baseRepository.manager,
+            baseRepository.queryRunner,
+          );
+        },
+      });
+    }
+
+    return {
+      exports: providers,
+      module: TypeormModule,
+      providers,
+    };
+  }
+}
